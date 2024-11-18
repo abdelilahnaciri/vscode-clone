c33cc21add41e53e650e7af9e376b9a869ff3090
abdelilahnaciri <abdelilahnaciri1221@gmail.com>
2024-11-18 22:23:46 +0100

    refactoring extension icon paths for files and folders

--- src/components/RenderFileIcon.tsx
+++ src/components/RenderFileIcon.tsx
@@ -1,3 +1,4 @@
+import { extensionIconPaths } from "../constant";
 import IconImg from "./IconImg";
 import FileIcon from "./SVG/File";
 
@@ -10,37 +11,18 @@ interface IProps {
 const RenderFileIcon = ({ filename, isFolder, isOpen }: IProps) => {
   const extension = filename.split(".").pop();
 
-  // ** Files:
-  if (extension === "tsx") return <IconImg src={"icons/react_ts.svg"} />;
-  if (extension === "jsx") return <IconImg src={"icons/react.svg"} />;
-  if (extension === "js") return <IconImg src={"icons/javascript.svg"} />;
-  if (extension === "html") return <IconImg src={"icons/html.svg"} />;
+  if (
+    extension &&
+    Object.prototype.hasOwnProperty.call(extensionIconPaths, extension)
+  ) {
+    const iconPath = isFolder
+      ? isOpen
+        ? `${extensionIconPaths[extension]}-open.svg`
+        : `${extensionIconPaths[extension]}.svg`
+      : `${extensionIconPaths[extension]}.svg`;
+    return <IconImg src={iconPath} />;
+  }
 
-  // ** Folders:
-  if (extension === "node_modules" && isFolder)
-    return isOpen ? (
-      <IconImg src={"icons/folder-node-open.svg"} />
-    ) : (
-      <IconImg src={"icons/folder-node.svg"} />
-    );
-  if (extension === "public" && isFolder)
-    return isOpen ? (
-      <IconImg src={"icons/folder-public-open.svg"} />
-    ) : (
-      <IconImg src={"icons/folder-public.svg"} />
-    );
-  if (extension === "src" && isFolder)
-    return isOpen ? (
-      <IconImg src={"icons/folder-src-open.svg"} />
-    ) : (
-      <IconImg src={"icons/folder-src.svg"} />
-    );
-  if (extension === "components" && isFolder)
-    return isOpen ? (
-      <IconImg src={"icons/folder-components-open.svg"} />
-    ) : (
-      <IconImg src={"icons/folder-components.svg"} />
-    );
   if (isFolder && isOpen)
     return <IconImg src={"icons/folder-default-open.svg"} />;
   if (isFolder && !isOpen) return <IconImg src={"icons/folder-default.svg"} />;
---
+++ src/constant/index.ts
@@ -0,0 +1,13 @@
+export const extensionIconPaths: Record<string, string> = {
+  // ** Files:
+  tsx: "icons/react_ts",
+  jsx: "icons/react",
+  js: "icons/javascript",
+  html: "icons/html",
+
+  // ** Folders:
+  node_modules: "icons/folder-node",
+  public: "icons/folder-public",
+  src: "icons/folder-src",
+  components: "icons/folder-components",
+};
